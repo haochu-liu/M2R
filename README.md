adc9f4b08bc1f319f829133bb1a9b5aa9b21f954
Haochu <Liu_haochu@outlook.com>
2022-06-01 10:55:31 +0100

    modularity

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ Year 2 group project
 
 ### scale_free
 * Use Barabási-Albert model to generate secle free graph.
+
+### small_world
+* Use Watts Strogatz model to generate graph with small-world property. (not very sure)
+
+### modularity
+* Stochastic block model partitions the nodes in blocks.
+* We can use it to study the properties about modularity.
--- modularity/stochastic_block_model.py
+++ modularity/stochastic_block_model.py
@@ -1,3 +1,44 @@
+from SIR_Network_Simulation import Simulation
 import matplotlib.pyplot as plt
 import networkx as nx
+import numpy as np
+from scipy.linalg import eigh
 
+sizes = [40, 60]
+probs = [[0.2, 0.01], [0.01, 0.2]]
+
+G_sb = nx.stochastic_block_model(sizes, probs)
+
+
+def plot_graph(G):
+    plt.figure(1)
+    nx.draw(G, node_size=6)
+    plt.show()
+
+
+def spectral_modularity(G):
+    B = nx.modularity_matrix(G)
+    N = G.number_of_nodes()
+    e1, v1 = eigh(B, eigvals=(N-1, N-1))
+    e1 = e1[0]
+    v1 = v1[:, 0]
+    s = np.ones_like(v1)
+    s[v1 < 0] = -1
+    ind1p = np.where(s == 1)[0]
+    ind1m = np.where(s == -1)[0]
+    cSpectral = [set(ind1p), set(ind1m)]
+    Mspectral = nx.algorithms.community.modularity(G, cSpectral)
+    print("spectral modularity=", Mspectral)
+    return cSpectral
+
+
+def greedy_modularity(G):
+    cGreedy = list(nx.algorithms.community.greedy_modularity_communities(G))
+    Mgreedy = nx.algorithms.community.modularity(G, cGreedy)
+    print("greedy modularity=", Mgreedy)
+    return cGreedy
+
+
+S = Simulation(G_sb, 0.2, 0.1)
+S.infect_list([i for i in range(2)])
+S.continuous_display()

73e6d3655f99947f55a45a0eaedca2e2fa864f6f
Haochu <Liu_haochu@outlook.com>
2022-06-09 13:23:27 +0100

    ER and SBM

--- modularity/ER_and_SBM_dist.py
+++ modularity/ER_and_SBM_dist.py
@@ -17,7 +17,7 @@ prob_ER = p_SBM
 
 def sir(G, r):
     initial_size = random.randint(0, N)
-    tau = 1.
+    tau = 0.1
     gamma = r * tau
     t, S, I, R = EoN.fast_SIR(G, tau, gamma,
                               initial_infecteds=initial_size,
@@ -51,18 +51,23 @@ for i in range(len(k_SBM)):
         if find_t(t, S):
             d_SBM[i].append(find_t(t, S))
 
-
-plt.hist(d_ER, bins=10, color='c', edgecolor='k', alpha=0.5, density=True)
-plt.axvline(x=np.mean(d_SBM[0]))
-plt.axvline(x=np.mean(d_SBM[1]))
-plt.axvline(x=np.mean(d_SBM[2]))
-
 mu = np.mean(d_ER)
 var = np.var(d_ER)
-x = np.linspace(0, np.mean(d_SBM[2]), 500)
-y = stats.norm.pdf(x, loc=mu, scale=sqrt(var))
-plt.plot(x, y)
+theta = var / mu
+k = mu / theta
+x = np.linspace(0, 3, 300)
+y = stats.gamma.pdf(x, k, scale=theta)
+plt.plot(x, y, color='c')
+p1 = stats.gamma.cdf(np.mean(d_SBM[0]), k, scale=theta)
+p2 = stats.gamma.cdf(np.mean(d_SBM[1]), k, scale=theta)
+p3 = stats.gamma.cdf(np.mean(d_SBM[2]), k, scale=theta)
+
+plt.hist(d_ER, bins=20, color='b', edgecolor='none', alpha=0.6, density=True)
+plt.axvline(x=np.mean(d_SBM[0]), color='k', linestyle='-.', label='mean $t_{SBM}}$ of k = 0.1')
+plt.axvline(x=np.mean(d_SBM[1]), color='k', linestyle='--', label='mean $t_{SBM}$ of k = 0.01')
+plt.axvline(x=np.mean(d_SBM[2]), color='k', linestyle=':', label='mean $t_{SBM}$ of k = 0.001')
+plt.xlabel('t')
+plt.ylabel('Density')
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=1, fancybox=True, shadow=False)
 plt.show()
-print(stats.norm.cdf(np.mean(d_SBM[0]), loc=mu, scale=sqrt(var)))
-print(stats.norm.cdf(np.mean(d_SBM[1]), loc=mu, scale=sqrt(var)))
-print(stats.norm.cdf(np.mean(d_SBM[2]), loc=mu, scale=sqrt(var)))
+print(1-p1, 1-p2, 1-p3)
